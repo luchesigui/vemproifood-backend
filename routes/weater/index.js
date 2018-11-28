7671127a1c59a5eb15091d27ca41a5f97df892d0
Guilherme Luchesi <gui.olhenrique@gmail.com>
2018-11-28 01:33:28 -0200

    Adiciona funcao que transforma kelvin em celcius

--- routes/weater/index.js
+++ routes/weater/index.js
@@ -1,7 +1,9 @@
 const openWeatherRootUrl = 'http://api.openweathermap.org/data/2.5/weather'
 const openWeatherApiKey = 'b77e07f479efe92156376a8b07640ced'
+const kelvinToCelcius = kelvin => Math.round(kelvin - 273.15)
 
 module.exports = {
   openWeatherRootUrl,
   openWeatherApiKey,
+  kelvinToCelcius,
 }

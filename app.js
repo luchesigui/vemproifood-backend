56676d7863b18dfb4f0cc5f124f0440f5ebdcacb
Guilherme Luchesi <gui.olhenrique@gmail.com>
2018-11-28 01:41:43 -0200

    Remove rota de usuario e raiz e adiciona rota de tracks

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -25,8 +24,7 @@ app.all('/*', function(req, res, next) {
 	next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tracks', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

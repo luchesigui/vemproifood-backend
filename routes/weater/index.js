ab4a3a123d960034b33b2ef2a4f39115ccd6e3d6
Guilherme Luchesi <gui.olhenrique@gmail.com>
2018-11-28 01:32:32 -0200

    Adiciona variavies da api do open weater

---
+++ routes/weater/index.js
@@ -0,0 +1,7 @@
+const openWeatherRootUrl = 'http://api.openweathermap.org/data/2.5/weather'
+const openWeatherApiKey = 'b77e07f479efe92156376a8b07640ced'
+
+module.exports = {
+  openWeatherRootUrl,
+  openWeatherApiKey,
+}
